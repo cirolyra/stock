ddd169cb386c581ed6b3aaebda48f565aef8a490
cirol <cirol@192.168.147.1>
2021-05-19 00:55:14 -0300

    Criando uma condição para entrada e saída no estoque principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,21 +22,37 @@ public class Program {
 		produto.quantidade = sc.nextInt();
 
 		System.out.println(produto);
-		
+
 		System.out.println();
-		System.out.println("Adição no estoque: ");
-		int quantidade = sc.nextInt();
-		produto.AddProducts(quantidade);
-		
-		System.out.println(produto);
-		
+		System.out.println("Deseja realizar adição no estoque (S/N): ");
+		char resp = sc.next().charAt(0);
+
+		if (resp != 'n') {
+
+			System.out.println();
+			System.out.println("Adição no estoque: ");
+			int quantidade = sc.nextInt();
+			produto.AddProducts(quantidade);
+
+			System.out.println(produto);
+
+		}
+
 		System.out.println();
-		System.out.println("Remoção no estoque: ");
-		quantidade = sc.nextInt();
-		produto.RemoveProducts(quantidade);
-		
-		System.out.println(produto);
-		
+		System.out.println("Deseja realizar remoção no estoque (S/N): ");
+		resp = sc.next().charAt(0);
+
+		if (resp != 'n') {
+
+			System.out.println();
+			System.out.println("Remoção no estoque: ");
+			int quantidade = sc.nextInt();
+			produto.RemoveProducts(quantidade);
+
+			System.out.println(produto);
+
+		}
+
 		sc.close();
 	}
 
